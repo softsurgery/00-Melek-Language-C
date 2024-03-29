7175cadc0fe20dd9d4921077c2f5739e48e2688d
softsurgery <houssemeddine.chibouni@fsb.ucar.tn>
2024-03-29 13:41:36 +0100

    feat(wagon update)

--- C Language/LinkedLists/Train/main.c
+++ C Language/LinkedLists/Train/main.c
@@ -17,16 +17,20 @@ typedef struct {
     noeud* tete;
 } liste;
 
+//typedef noeud* liste
+
 void ajourerLocomotive(liste* l){
-    noeud* nouveau = (noeud*) malloc(sizeof(noeud));
+    if (l->tete == NULL || strcmp(l->tete->w.type,"locomotive")!= 0 ){
+        noeud* nouveau = (noeud*) malloc(sizeof(noeud));
+        printf("Numero = ");
+        scanf("%d",&nouveau->w.num);
+        strcpy(nouveau->w.type,"locomotive");
+        nouveau->w.etat=1;
 
-    printf("Numero = ");
-    scanf("%d",&nouveau->w.num);
-    strcpy(nouveau->w.type,"locomotive");
-    nouveau->w.etat=1;
-
-    nouveau->suivant=l->tete;
-    l->tete=nouveau;
+        nouveau->suivant=l->tete;
+        l->tete=nouveau;
+    } else printf("On peut pas ajouter une locomotive");
+   
 }
 
 void ajouterWagon(liste* l){
@@ -66,6 +70,79 @@ void afficheTrain(liste l){
     while(p!= NULL){
         printf("Wagon [Num = %d, etat = %s]\n", p->w.num, p->w.type == 1? "OK" : "en panne");
         p = p->suivant;
+    }
 }
 
+void supprimer_en_panne(liste* l){
+    if (l->tete != NULL){
+        noeud  *prec=l->tete, *p = l->tete->suivant;
+        while(p!=NULL) {
+            while(p->w.type!=0){
+                prec = p;
+                p = p->suivant;
+            }
+            prec->suivant = p->suivant;
+            free(p);
+            p = prec->suivant;
+        }
+        if (l->tete->w.type==0){
+            p = l->tete;
+            l->tete = l->tete->suivant;
+            free(p);
+        }
+    } 
+    
+}
+
+void ajouter_en_position(liste* l, int pos){
+    if (pos < 0) printf("Postion -");
+    else {
+        noeud* p = l-> tete;
+        while(p!=NULL && pos!=0){
+            pos--;
+            p = p->suivant;
+        }
+        if (p!=NULL && pos!=0) printf("Postion ++");
+        else {
+            noeud* nouveau = (noeud*) malloc(sizeof(noeud));
+            printf("Numero = ");
+            scanf("%d",&nouveau->w.num);
+            strcpy(nouveau->w.type,"wagon");
+            nouveau->w.etat=1;
+
+            if (l->tete == NULL){
+                nouveau->suivant=l->tete;
+                l->tete=nouveau;
+            } else {
+                nouveau->suivant = p->suivant;
+                p->suivant=nouveau;
+            }
+        }
+
+    }
+}
+
+void ajouter_apres_wagon(liste* l,int num,wagon w){
+    noeud* nouveau = (noeud*) malloc(sizeof(noeud));
+    nouveau->w = w;
+    noeud* p = l->tete;
+    while(p!=NULL && p->w.num!=num) p = p->suivant;
+    if (p!=NULL) {
+        nouveau->suivant = p->suivant;
+        p->suivant=nouveau;
+    }
+}
+
+void purge(liste* l){
+    if (l->tete !=NULL){
+        noeud* prec = l->tete;
+        noeud* p = l->tete->suivant;
+        while(p!=NULL){
+            free(prec);
+            prec = p;
+            p = p->suivant;
+        }
+        free(prec);
+    }
+}
 
